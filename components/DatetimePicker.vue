6ac4773a9c860407955f3d19c4f6903e1f7a798b
flukkyz555 <kunakorn.flukky@gmail.com>
2024-04-21 15:56:05 +0700

    update datertime picker

--- components/DatetimePicker.vue
+++ components/DatetimePicker.vue
@@ -11,11 +11,7 @@ const props = withDefaults(
     range?: boolean;
     minDate?: string | Date;
     maxDate?: string | Date;
-    placeholder?: string;
-    prefix?: string;
     prefixIcon?: string;
-    suffix?: string;
-    suffixIcon?: string;
     dateOnly?: boolean;
     enableSeconds?: boolean;
     beYear?: boolean;
@@ -42,6 +38,29 @@ const openCalendar = () => {
     datepicker.value?.openMenu();
   }
 };
+
+const renderValue = computed(() => {
+  if (props.modelValue) {
+    if (props.range && isArray(props.modelValue)) {
+      return `${datetime.toText(
+        props.modelValue[0],
+        "medium",
+        props.dateOnly ? null : props.enableSeconds ? "long" : "short"
+      )} - ${datetime.toText(
+        props.modelValue[1],
+        "medium",
+        props.dateOnly ? null : props.enableSeconds ? "long" : "short"
+      )}`;
+    } else {
+      return datetime.toText(
+        props.modelValue as string,
+        "medium",
+        props.dateOnly ? null : props.enableSeconds ? "long" : "short"
+      );
+    }
+  }
+  return "";
+});
 
 const emits = defineEmits<{
   (e: "update:modelValue", value: null | string | string[]): void;
@@ -86,137 +105,67 @@ const onClearable = (): void => {
 </script>
 
 <template>
-  <div
-    class="flex items-center rounded-md border bg-white border-gray-300 shadow-sm focus-within:ring-1 focus-within:border-primary-600 focus-within:ring-primary-600"
-    :class="[
-      disabled
-        ? 'cursor-not-allowed border-gray-200 bg-gray-100 text-gray-500'
-        : '',
-    ]"
+  <UInput
+    :model-value="renderValue"
+    v-bind="{ ...$attrs }"
+    readonly
+    @click="openCalendar"
   >
-    <slot name="prefix">
-      <div
-        :class="[
-          'flex items-center flex-shrink-0',
-          disabled
-            ? 'disabled:cursor-not-allowed disabled:text-gray-300'
-            : 'cursor-pointer',
-        ]"
+    <template #leading>
+      <UButton
+        color="gray"
+        size="xs"
+        variant="link"
+        :icon="prefixIcon"
+        :padded="false"
         @click="openCalendar"
-      >
-        <div
-          v-if="prefixIcon"
-          class="h-5 w-5 flex items-center justify-center ml-2"
-        >
-          <UIcon
-            :name="prefixIcon"
-            :class="[disabled ? 'text-gray-300' : 'text-gray-500']"
-          />
-        </div>
-        <span v-if="prefix" class="text-gray-500 sm:text-sm ml-2">
-          {{ prefix }}
-        </span>
-      </div>
-    </slot>
-
-    <Datepicker
-      :model-value="modelValue"
-      ref="datepicker"
-      :range="range"
-      :multi-calendars="range"
-      :readonly="readonly"
-      :disabled="disabled"
-      :auto-apply="dateOnly"
-      :min-date="minDate"
-      :max-date="maxDate"
-      :locale="useFind(locales, { code: locale })!.iso"
-      :select-text="$t('SELECT')"
-      :cancel-text="$t('CANCEL')"
-      :action-row="{ showNow: true }"
-      :now-button-label="dateOnly ? $t('TODAY') : $t('NOW')"
-      :placeholder="placeholder"
-      time-picker-inline
-      :clearable="false"
-      :enable-time-picker="!dateOnly"
-      :enable-seconds="enableSeconds"
-      @update:model-value="input"
-      @blur="blur"
-    >
-      <template #year="{ value }">
-        {{ beYear || locale === "th" ? parseInt(value) + 543 : value }}
-      </template>
-      <template #year-overlay-value="{ text }">
-        <div>
-          {{ beYear || locale === "th" ? parseInt(text) + 543 : text }}
-        </div>
-      </template>
-      <template #dp-input="{ value }">
-        <div
-          class="block w-full border-0 p-0 bg-transparent pt-2 pb-1.5 placeholder-gray-400 focus:ring-0 sm:text-sm mx-2 focus:outline-none"
-          :class="[
-            disabled
-              ? 'disabled:cursor-not-allowed disabled:border-gray-200 disabled:text-gray-500'
-              : 'cursor-pointer text-gray-900',
-          ]"
-        >
-          <span v-if="value" class="font-normal">
-            <span v-if="range">
-              {{
-                datetime.toText(
-                  value.split(" - ")[0],
-                  "medium",
-                  dateOnly ? null : enableSeconds ? "long" : "short"
-                )
-              }}
-              -
-              {{
-                datetime.toText(
-                  value.split(" - ")[1],
-                  "medium",
-                  dateOnly ? null : enableSeconds ? "long" : "short"
-                )
-              }}
-            </span>
-            <span v-else>
-              {{
-                datetime.toText(
-                  value,
-                  "medium",
-                  dateOnly ? null : enableSeconds ? "long" : "short"
-                )
-              }}
-            </span>
-          </span>
-          <span class="text-gray-400 text-sm" v-else
-            >{{ placeholder }}&nbsp;</span
-          >
-        </div>
-      </template>
-    </Datepicker>
-    <div
-      v-if="clearable && modelValue && !disabled"
-      class="h-5 w-5 flex items-center justify-center mr-2"
-    >
-      <UIcon
-        name="i-fa6-regular-circle-xmark"
-        class="text-gray-300 hover:text-gray-dark cursor-pointer"
-        @click="onClearable()"
       />
-    </div>
-    <slot name="suffix">
-      <div class="pointer-events-none flex items-center flex-shrink-0">
-        <span v-if="suffix" class="text-gray-500 sm:text-sm mr-2">
-          {{ suffix }}
-        </span>
-        <div
-          v-if="suffixIcon"
-          class="h-5 w-5 flex items-center justify-center mr-2"
-        >
-          <i :class="suffixIcon" class="text-gray-500" />
-        </div>
+    </template>
+    <template #trailing>
+      <UButton
+        v-show="clearable && modelValue && !disabled"
+        color="gray"
+        size="xs"
+        variant="link"
+        icon="i-fa6-solid-xmark"
+        :padded="false"
+        @click="onClearable"
+      />
+    </template>
+  </UInput>
+
+  <Datepicker
+    :model-value="modelValue"
+    ref="datepicker"
+    :range="range"
+    :multi-calendars="range"
+    :readonly="readonly"
+    :disabled="disabled"
+    :auto-apply="dateOnly"
+    :min-date="minDate"
+    :max-date="maxDate"
+    :locale="useFind(locales, { code: locale })!.iso"
+    :select-text="$t('SELECT')"
+    :cancel-text="$t('CANCEL')"
+    :action-row="{ showNow: true }"
+    :now-button-label="dateOnly ? $t('TODAY') : $t('NOW')"
+    time-picker-inline
+    :clearable="false"
+    :enable-time-picker="!dateOnly"
+    :enable-seconds="enableSeconds"
+    @update:model-value="input"
+    @blur="blur"
+  >
+    <template #year="{ value }">
+      {{ beYear || locale === "th" ? parseInt(value) + 543 : value }}
+    </template>
+    <template #year-overlay-value="{ text }">
+      <div>
+        {{ beYear || locale === "th" ? parseInt(text) + 543 : text }}
       </div>
-    </slot>
-  </div>
+    </template>
+    <template #dp-input />
+  </Datepicker>
 </template>
 
 <style>
